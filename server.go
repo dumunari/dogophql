d94a11f719d927c9baeeb87733a0d9554611a1d8
Eduardo Munari <munari.edu@gmail.com>
2021-07-18 14:53:07 -0300

    add initial data to facilitate tests

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/dumunari/dogophql/graph/model"
 	"log"
 	"net/http"
 	"os"
@@ -13,13 +14,14 @@ import (
 
 const defaultPort = "8080"
 
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: setupInitialConfiguration()}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/dogophql"))
 	http.Handle("/dogophql", srv)
@@ -27,3 +29,51 @@ func main() {
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
+
+func setupInitialConfiguration() *graph.Resolver {
+	a := 1
+	c := "Black"
+	breed := &model.Breed{
+		ID:   "T5577006791947779410",
+		Name: "English bulldog",
+	}
+	p1 := &model.Dog{
+		ID:      "T6129484611666145821",
+		Name:    "Spike",
+		Age:     &a,
+		Breed:   breed,
+		Puppies: nil,
+	}
+
+	p2 := &model.Dog{
+		ID:      "T2775422040480279449",
+		Name:    "Spika",
+		Age:     &a,
+		Breed:   breed,
+		Puppies: nil,
+	}
+	puppy := &model.Puppy{
+		ID:      "T4751997750760398084",
+		Name:    "Tyke",
+		Color:   &c,
+		Breed:   breed,
+		Parents: []*model.Dog{
+			p1,
+			p2,
+		},
+	}
+
+	return &graph.Resolver{
+		Dogs:    []*model.Dog{
+			p1,
+			p2,
+		},
+		Breeds:  []*model.Breed{
+			breed,
+		},
+		Puppies: []*model.Puppy{
+			puppy,
+		},
+	}
+}
+
